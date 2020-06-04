da9210194b58dceedb80be4d4edd1d4fdecfdf0e
vaidyanathanms <msvaidyanathan1729@gmail.com>
2020-06-04 11:58:58 -0400

    V1.1 -- bug fixes: on directory names, added min/max time for analysis.

--- src_matlab/adsfrac.m
+++ src_matlab/adsfrac.m
@@ -14,14 +14,15 @@ lsty = {'-','--',':'};
 msty = {'d','s','o','x'};
 
 %% Inputs
-nfreearr = [16;32;48;64;72;80;100];
+nfreearr = [16;32;64;72;80;100];
 casearr  = [1,2,3,4];
 pdi_freearr = [1,1.3,1.5];
-arch_arr = {'bl_bl';'bl_al';'al_bl';'al_al'};
+arch_arr = {'bl_bl','bl_al','al_bl','al_al'};
 pdigraft = 1.0;
 nmonfree = 30; nmongraft = 30; ngraft = 64;
 cutoff = '1.50';
 lz = 120; area=35^2;
+set_tmax = 3e7; % maximum timestep for analysis;
 
 %% Input flags
 stddevon = 0;
@@ -51,19 +52,27 @@ for pdi_cntr = 1:length(pdi_freearr) % begin pdi free loop
     for ncnt = 1:length(nfreearr) % begin nfree loop
         nval = nfreearr(ncnt);
         
-        for i = length(arch_arr)  % begin arch loop
-            dirstr = arch_arr{i};
+        for arch_cnt = 1:length(arch_arr)  % begin arch loop
+            dirstr = arch_arr{arch_cnt};
+            
+            dirname = sprintf('./../../sim_results/outresults_dir_n_%d/%s/pdifree_%s_pdigraft_%s',...
+                nval,dirstr,pdifree_str,pdigraft_str);
+            
+            if ~exist(dirname,'dir')
+                fprintf('%s does not exist\n',dirname);
+                continue
+            end
             
             if adsflag % create case-based avg outfiles
                 fout_case = fopen(sprintf('./../../outfiles/n_%d/adsorbed_chain_rcut_%s_pdifree_%g_%s.dat',...
                     nval,cutoff,pdifree,dirstr),'w');
-                fprintf(fout_case,'%s\t%s\t%s\t%s\n','N_f','Casenum','numavgpoints','avg_fraction');
+                fprintf(fout_case,'%s\t%s\t%s\t%s\t%s\t%s\n','N_f','Casenum','Mintime','Maxtime','numavgpoints','avg_fraction');
             end
             
             for casecntr = 1:length(casearr) % begin case loop
                 casenum = casearr(casecntr);
                 
-                dirname = sprintf('./../../sim_results/ouresults_n_%d/%s/pdifree_%s_pdigraft_%s/Case_%d',...
+                dirname = sprintf('./../../sim_results/outresults_dir_n_%d/%s/pdifree_%s_pdigraft_%s/Case_%d',...
                     nval,dirstr,pdifree_str,pdigraft_str,casenum);
                 
                 if ~exist(dirname,'dir')
@@ -71,11 +80,12 @@ for pdi_cntr = 1:length(pdi_freearr) % begin pdi free loop
                     continue
                 end
                 
-                if adsflag %begin adsorption calculation
+                fprintf('Analyzing pdi/nfree/arch/case: %g\t%d\t%s\t%d\n', pdifree,nval,dirstr,casenum);
+                if adsflag % begin adsorption calculation
                     
                     %check if file type exists
                     ads_prefix = sprintf('adsfracchain_rcut_%s_config_*.lammpstrj',cutoff);
-                    ads_fylelist = dir(strcat(simdirname,'/',ads_prefix));
+                    ads_fylelist = dir(strcat(dirname,'/',ads_prefix));
                     if min(size(ads_fylelist)) == 0
                         fprintf('No files/Empty files are found for %s\n',ads_prefix);
                         continue;
@@ -83,9 +93,9 @@ for pdi_cntr = 1:length(pdi_freearr) % begin pdi free loop
                     
                     nfyles = numel(ads_fylelist); %number of files of the type
                     
-                    sum_across_files = 0; tot_cntr_across_files = 0;
+                    sum_across_files = 0; tot_cntr_across_files = 0; mintime = 0; maxtime = 0;
                     for fylcnt = 1:nfyles % begin running through all files of the given type
-                        ads_fylename = fylelist(fylcnt).name;
+                        ads_fylename = strcat(dirname,'/',ads_fylelist(fylcnt).name);
                         if exist(ads_fylename,'file') ~= 2
                             fprintf('%s does not exist/empty file\n',ads_fylename);
                             continue;
@@ -94,26 +104,41 @@ for pdi_cntr = 1:length(pdi_freearr) % begin pdi free loop
                             continue;
                         end
                         
+                        %average adsorption values
+                        fprintf('Analyzing %s\n', ads_fylename);
                         data = importdata(ads_fylename);
                         nads_fracchain = sum(data(:,3));
                         sum_across_files = sum_across_files + nads_fracchain;
                         tot_cntr_across_files = tot_cntr_across_files + length(data(:,3));
+                        
+                        %find minimum and maximum time
+                        if min(data(:,1)) < mintime
+                            mintime = min(data(:,1));
+                        end
+                        if max(data(:,1)) < mintime
+                            maxtime = max(data(:,1));
+                        end
+                        
                     end % end summing adsfrac across all files
                     
                     avg_for_each_casenum = sum_across_files/tot_cntr_across_files;
-                    fprintf(fout_case,'%d\t%s\t%d\t%g\n',nval,casenum,tot_cntr_across_files,avg_for_each_casenum);
+                    if maxtime > set_tmax
+                        fprintf(fout_case,'%d\t%s\t%d\t%d\t%d\t%g\n',nval,casenum,mintime,maxtime,tot_cntr_across_files,avg_for_each_casenum);
+                    else
+                        fprintf(fout_case,'%d\t%s\t%d\t%d\t%d\t%g\t%s\n',nval,casenum,mintime,maxtime,tot_cntr_across_files,avg_for_each_casenum,'Incomplete sampling');
+                    end
                     
                     %save it to overall arrays
-                    casecntr_arr(ncnt,i)  = casecntr_arr(ncnt,i) + 1;
-                    nadschain_all(ncnt,i) = nadschain_all(ncnt,i) + avg_for_each_casenum;
-                    totsamples(ncnt,i)    = totsamples(ncnt,i) + tot_cntr_across_files;
+                    casecntr_arr(ncnt,arch_cnt)  = casecntr_arr(ncnt,arch_cnt) + 1;
+                    nadschain_all(ncnt,arch_cnt) = nadschain_all(ncnt,arch_cnt) + avg_for_each_casenum;
+                    totsamples(ncnt,arch_cnt)    = totsamples(ncnt,arch_cnt) + tot_cntr_across_files;
                     
                 end %end adsorption calculation
                 
             end % end case loop
     
-            avg_across_cases = nadschain_all(ncnt,i)/casecntr_arr(ncnt,i);
-            fprintf(fout_avg,'%d\t%s\t%d\t%d\t%g\n',nval,dirstr,casecntr_arr(ncnt,i),totsamples(ncnt,i),avg_across_cases);
+            avg_across_cases = nadschain_all(ncnt,arch_cnt)/casecntr_arr(ncnt,arch_cnt);
+            fprintf(fout_avg,'%d\t%s\t%d\t%d\t%g\n',nval,dirstr,casecntr_arr(ncnt,arch_cnt),totsamples(ncnt,arch_cnt),avg_across_cases);
             
         end % end arch loop
         
