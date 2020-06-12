20c586b1da2c5d8432a90ab920a8592b14b04739
vaidyanathanms <msvaidyanathan1729@gmail.com>
2020-06-12 18:22:15 -0500

    changed if/elseif order to read num_atoms/Atoms part. This will make sure that the code doesn't quit before reading Atoms part. Changed for to while loop to make sure that blank lines are read

--- src_matlab/analyze_datafile.m
+++ src_matlab/analyze_datafile.m
@@ -1,4 +1,4 @@
-function [molcnt] = analyze_datafile(dataname,nfree_ch)
+function [molcnt_arr] = analyze_datafile(dataname,nfree_ch)
 
 % Check whether file exists
 fid = fopen(dataname,'r');
@@ -10,7 +10,7 @@ end
 
 fprintf('Analyzing %s\n', dataname);
 natom_flag = -1; % to see natoms keyword is read before reading Atoms
-molcnt = zeros(nfree_ch,1);
+molcnt_arr = zeros(nfree_ch,1);
 
 % Start reading file
 fgetl(fid); % skip first line;
@@ -22,29 +22,39 @@ while true
         continue;
     end
     spl_tline = strsplit(strtrim(tline));
-        
-    if length(spl_tline) > 1 % for double keywords check this first
-        if strcmp(spl_tline{2},'atoms') % check for atoms keyword
-        
-            num_atoms = str2double(spl_tline{1});
-            natom_flag = 1;
-            
-        end
-        
-    elseif strcmp(spl_tline{1},'Atoms') % check for Atoms keyword
+    
+    if strcmp(spl_tline{1},'Atoms') % check for Atoms keyword
         
         if natom_flag == -1
             fprintf('%s Did not find natoms keyword in', dataname);
             return
         end
         
-        for i = 1:num_atoms % increment molcnt counter based on molid
-            tline = fgetl(fid); spl_tline = strsplit(strtrim(tline));
-            molid = str2double(spl_tline{2});
-            molcnt(molid) = molcnt(molid) + 1;
+        i = 1;
+        while (i <= num_atoms) % increment molcnt counter based on molid
+            
+            tline = fgetl(fid);
+            spl_tline = strsplit(strtrim(tline))
+            if ~ischar(tline) || isempty(tline)
+                continue;
+            end
+            molid = str2double(spl_tline{2})
+            molcnt_arr(molid) = molcnt_arr(molid) + 1;
+            i = i + 1;
+            
         end
         
+        molcnt_arr
         break; % no need to read the rest of the file
+        
+    elseif length(spl_tline) > 1 % for double keywords check this first
+        
+        if strcmp(spl_tline{2},'atoms') % check for atoms keyword
+            
+            num_atoms = str2double(spl_tline{1});
+            natom_flag = 1;
+            
+        end % do this "ELSEIF" condition after the first "IF" condition, so that the elseif statement works correctly.
         
     end
     
