26246cca26066b955e7f8c15914c8a313dc5d75a
vaidyanathanms <msvaidyanathan1729@gmail.com>
2020-06-12 18:42:13 -0500

    current version accounts for graft monomers in the datafile before the free monomers. needs corresponding input.

--- src_matlab/analyze_datafile.m
+++ src_matlab/analyze_datafile.m
@@ -1,4 +1,6 @@
-function [molcnt_arr] = analyze_datafile(dataname,nfree_ch)
+function [molcnt_arr] = analyze_datafile(dataname,nfree_ch,ngraft_ch)
+
+% Order in datafile: graft mons -> free mons -> counter ions -> salt
 
 % Check whether file exists
 fid = fopen(dataname,'r');
@@ -8,9 +10,11 @@ if fid <= 0
     return
 end
 
+% Initialize data
 fprintf('Analyzing %s\n', dataname);
 natom_flag = -1; % to see natoms keyword is read before reading Atoms
-molcnt_arr = zeros(nfree_ch,1);
+molcnt_arr = zeros(nfree_ch,3);
+max_chain_id  = ngraft_ch + nfree_ch; % maximum range of mol list from file read
 
 % Start reading file
 fgetl(fid); % skip first line;
@@ -34,12 +38,24 @@ while true
         while (i <= num_atoms) % increment molcnt counter based on molid
             
             tline = fgetl(fid);
-            spl_tline = strsplit(strtrim(tline))
+            spl_tline = strsplit(strtrim(tline));
             if ~ischar(tline) || isempty(tline)
                 continue;
             end
-            molid = str2double(spl_tline{2})
-            molcnt_arr(molid) = molcnt_arr(molid) + 1;
+            
+            if fix(str2double(spl_tline{2})) > ngraft_ch && ...
+                    fix(str2double(spl_tline{2})) <= max_chain_id %look at only free mons
+                
+                molid = str2double(spl_tline{2});
+                remap_molid = molid - ngraft_ch;
+                
+                molcnt_arr(remap_molid,1) = molid; %actual molid in datafile
+                molcnt_arr(remap_molid,2) = remap_molid; %remapped id
+                molcnt_arr(remap_molid,3) = molcnt_arr(remap_molid,3) + 1; %mw for a particular chain
+                
+            end
+            
+            
             i = i + 1;
             
         end
