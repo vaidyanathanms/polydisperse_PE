5e7f4b104cf8632012372ece102c460f7adc3078
vaidyanathanms <msvaidyanathan1729@gmail.com>
2020-06-15 17:14:27 -0500

    changed numbins to binwidth and added normalized probability

--- src_matlab/compute_mwdist.m
+++ src_matlab/compute_mwdist.m
@@ -4,13 +4,9 @@ function [distout] = compute_mwdist(molarr,column_num)
 % Inputs: array name and the column number for which distn needs to be
 % calculated.
 
-% Arbitrarily keep num of bins at one fourth the max mol. wt. 
-% So for uniform dist, every 4 mol.wt will contribute to one bin.
+% Keep bin width to 8.
 % Filter out ZERO molecular weight
 
-[~,~,filt_dist] = find(molarr(:,column_num))
-numbins   = floor(0.25*max(filt_dist))
-distout   = histogram(filt_dist,numbins);
-
-
-
+[~,~,filt_dist] = find(molarr(:,column_num));
+binwid    = 8;
+distout   = histogram(filt_dist,'BinWidth',binwid,'Normalization','pdf');
