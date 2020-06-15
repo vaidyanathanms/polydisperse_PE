101bfd22f4c370037a196dfe6ac5553e21863bd9
vaidyanathanms <msvaidyanathan1729@gmail.com>
2020-06-15 12:55:45 -0500

    bug fix: to add the maximum of the molecular weigth for computing the maximum binval

--- src_matlab/compute_mwdist.m
+++ src_matlab/compute_mwdist.m
@@ -1,11 +1,13 @@
-function [distout] = compute_mwdist(molarr)
+function [distout] = compute_mwdist(molarr,column_num)
 
 % To compute the molecular weight distribution.
+% Inputs: array name and the column number for which distn needs to be
+% calculated.
 
 % Arbitrarily keep num of bins at one fourth the max mol. wt. 
 % So for uniform dist, every 4 mol.wt will contribute to one bin.
-maxbinval = floor(0.25*molarr(:,3)); 
-distout = histogram(molarr(:,3),maxbinval);
+maxbinval = floor(0.25*max(molarr(:,column_num))); 
+distout   = histogram(molarr(:,column_num),maxbinval);
 
 
 
