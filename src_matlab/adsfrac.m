c69c09fa27b8220e738665b2157ace6c54f8cf40
vaidyanathanms <msvaidyanathan1729@gmail.com>
2020-06-16 22:14:08 -0500

    removed adsflag. it is redundant. also added the capability to write files for t-test analysis.

--- src_matlab/adsfrac.m
+++ src_matlab/adsfrac.m
@@ -18,15 +18,19 @@ nfreearr = [16;32;64;96;128;150];
 casearr  = [1,2,3,4];
 pdi_freearr = [1,1.3,1.5];
 arch_arr = {'bl_bl','bl_al','al_bl','al_al'};
+leg_arr  = {'Block-Block','Block-Alter','Alter-Block','Alter-Alter'}; % ALWAYS CHECK for correspondence with arch_arr
 pdigraft = 1.0;
-nmonfree = 30; nmongraft = 30; ngraft = 64;
+nmonfree = 30; nmongraft = 30; ngraft = 32;
 cutoff = '1.50';
 lz = 120; area=35^2;
 set_tmax = 3e7; % maximum timestep for analysis;
 
 %% Input flags
-stddevon = 0;
-adsflag  = 1;
+ttestflag = 0; % write to ttest_dir the individual cases
+plotads   = 1;
+
+%% Zero arrays
+avg_across_cases = zeros(length(nfreearr),length(arch_arr),length(pdi_freearr));
 
 %% Pre-calculations
 rhofree = nfreearr*30/(lz*area);
@@ -38,7 +42,7 @@ if adsflag % create consolidated list
     fout_cons = fopen(sprintf('./../../outfiles/overall/adsorbed_chain_consolidated_rcut_%s.dat',...
         cutoff),'w');
     fprintf(fout_cons,'%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n','PDI_free','N_f','Arch',...
-        'Case_num','numsample_pts','min_time','max_time','avg_fraction');
+        'Case_num','min_time','max_time','numsample_pts','avg_fraction');
 end
 
 
@@ -74,7 +78,14 @@ for pdi_cntr = 1:length(pdi_freearr) % begin pdi free loop
             if adsflag % create case-based avg outfiles
                 fout_case = fopen(sprintf('./../../outfiles/n_%d/adsorbed_chain_rcut_%s_pdifree_%g_%s.dat',...
                     nval,cutoff,pdifree,dirstr),'w');
-                fprintf(fout_case,'%s\t%s\t%s\t%s\t%s\t%s\n','N_f','Casenum','Mintime','Maxtime','numavgpoints','avg_fraction');
+                fprintf(fout_case,'%s\t%s\t%s\t%s\t%s\t%s\n','N_f','Casenum',...
+                    'Mintime','Maxtime','numavgpoints','avg_fraction');
+            end
+            
+            if ttestflag %write all cases into separate folders in ttest_dir
+                fout_ttest = fopen(sprintf('./../../outfiles/n_%d/adsfrac_rcut_%s_pdifree_%g_arch_%s.dat',...
+                    nval,cutoff,pdifree,dirstr),'w');
+                fprintf(fout_ttest,'%s\n','avg_fraction: #of columns correspond to number of cases');
             end
             
             for casecntr = 1:length(casearr) % begin case loop
@@ -101,7 +112,7 @@ for pdi_cntr = 1:length(pdi_freearr) % begin pdi free loop
                     
                     nfyles = numel(ads_fylelist); %number of files of the type
                     
-                    sum_across_files = 0; tot_cntr_across_files = 0; mintime = 0; maxtime = 0;
+                    sum_across_files = 0; tot_cntr_across_files = 0; mintime = 10^10; maxtime = 0;
                     for fylcnt = 1:nfyles % begin running through all files of the given type
                         ads_fylename = strcat(dirname,'/',ads_fylelist(fylcnt).name);
                         if exist(ads_fylename,'file') ~= 2
@@ -123,16 +134,17 @@ for pdi_cntr = 1:length(pdi_freearr) % begin pdi free loop
                         if min(data(:,1)) < mintime
                             mintime = min(data(:,1));
                         end
-                        if max(data(:,1)) < mintime
+                        if maxtime < max(data(:,1))
                             maxtime = max(data(:,1));
                         end
                         
                     end % end summing adsfrac across all files
                     
                     avg_for_each_casenum = sum_across_files/tot_cntr_across_files;
+                    
                     if maxtime > set_tmax
                         fprintf(fout_case,'%d\t%s\t%d\t%d\t%d\t%g\n',nval,casenum,...
-                            mintime,maxtime,tot_cntr_across_files,avg_for_each_casenum);   
+                            mintime,maxtime,tot_cntr_across_files,avg_for_each_casenum);
                         fprintf(fout_cons,'%g\t%d\t%s\t%d\t%d\t%d\t%d\t%g\n',pdifree,nval,...
                             dirstr,casenum,mintime,maxtime,tot_cntr_across_files,avg_for_each_casenum);
                     else
@@ -147,14 +159,18 @@ for pdi_cntr = 1:length(pdi_freearr) % begin pdi free loop
                     nadschain_all(ncnt,arch_cnt) = nadschain_all(ncnt,arch_cnt) + avg_for_each_casenum;
                     totsamples(ncnt,arch_cnt)    = totsamples(ncnt,arch_cnt) + tot_cntr_across_files;
                     
-                end %end adsorption calculation
+                    if ttestflag % add to corresponding ttest file
+                        fprintf(fout_ttest,'%g\t',avg_for_each_casenum);
+                    end
+                        
+                end % end adsorption calculation
                 
             end % end case loop
             
             fclose(fout_case);
             
-            avg_across_cases = nadschain_all(ncnt,arch_cnt)/casecntr_arr(ncnt,arch_cnt);
-            fprintf(fout_avg,'%d\t%s\t%d\t%d\t%g\n',nval,dirstr,casecntr_arr(ncnt,arch_cnt),totsamples(ncnt,arch_cnt),avg_across_cases);
+            avg_across_cases(ncnt,arch_cnt,pdi_cntr) = nadschain_all(ncnt,arch_cnt)/casecntr_arr(ncnt,arch_cnt);
+            fprintf(fout_avg,'%d\t%s\t%d\t%d\t%g\n',nval,dirstr,casecntr_arr(ncnt,arch_cnt),totsamples(ncnt,arch_cnt),avg_across_cases(ncnt,arch_cnt,pdi_cntr));
             
         end % end arch loop
         
@@ -162,6 +178,69 @@ for pdi_cntr = 1:length(pdi_freearr) % begin pdi free loop
     
     fclose(fout_avg);
     
+    if plotads
+        h1 = figure;
+        hold on
+        box on
+        set(gca,'FontSize',16)
+        xlabel('$N_{pa}/N_{pc}$','FontSize',20,'Interpreter','Latex')
+        ylabel('$f_{ads}$','FontSize',20,'Interpreter','Latex')
+        
+        for plcnt = 1:length(arch_arr)
+            plot(nfreearr/ngraft,avg_across_cases(:,plcnt,pdi_cntr),'color',pclr{plcnt},...
+                'Marker',msty{plcnt},'MarkerSize',8,'MarkerFaceColor',pclr{plcnt},'LineStyle','None')
+            legendinfo{plcnt} = leg_arr{plcnt};
+        end
+        %overlay y = x line
+        %         x = 0:0.5:5; y = x;
+        %         plot(x,y,'LineWidth',2,'Color',orange,'LineStyle','--')
+        %         ylim([min(min(avg_across_cases(:,:,pdi_cntr))) 1.2*max(max(avg_across_cases(:,:,pdi_cntr)))])
+        legend(legendinfo,'FontSize',16,'Location','Best')
+        legend boxoff
+        saveas(h1,sprintf('./../../all_figures/adsorbchain_rcut_%s_pdi_%g.png',cutoff,pdifree));
+        clear legendinfo
+    end
+    
 end % end pdi free loop
 
 fclose(fout_cons);
+
+
+%% Plot as a function of PDI for each nval
+
+if plotads
+    
+    for ncnt = 1:length(nfreearr) % begin nfree loop
+        nval = nfreearr(ncnt);
+        
+        h1 = figure;
+        hold on
+        box on
+        set(gca,'FontSize',16)
+        xlabel('PDI','FontSize',20,'Interpreter','Latex')
+        ylabel('$f_{ads}$','FontSize',20,'Interpreter','Latex')
+        
+        for plcnt = 1:length(arch_arr)  % begin arch loop
+            
+            dirstr = arch_arr{plcnt};            
+            data_to_plot = zeros(length(pdi_freearr),1);
+            for pdicntr = 1:length(pdi_freearr) 
+                data_to_plot(pdicntr) = avg_across_cases(ncnt,plcnt,pdicntr);
+            end
+            data_to_plot(isnan(data_to_plot)) = 0; %to avoid NaNs
+            
+            plot(pdi_freearr,data_to_plot,'color',pclr{plcnt},...
+                'Marker',msty{plcnt},'MarkerSize',8,'MarkerFaceColor',pclr{plcnt},'LineStyle','None')
+            legendinfo{plcnt} = leg_arr{plcnt};
+            
+        end
+        
+        legend(legendinfo,'FontSize',16,'Location','Best')
+        legend boxoff
+        saveas(h1,sprintf('./../../all_figures/adsorbchain_rcut_%s_nval_%g.png',cutoff,nval));
+        clear legendinfo
+        
+    end
+    
+end
+            
