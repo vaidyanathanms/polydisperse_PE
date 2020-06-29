cbc9ba878a3fc83a5025fdbb20aa06b7d1818232
vaidyanathanms <msvaidyanathan1729@gmail.com>
2020-06-29 18:22:11 -0400

    changed input variable name to dataarr from molarr

--- src_matlab/compute_mwdist.m
+++ src_matlab/compute_mwdist.m
@@ -1,4 +1,4 @@
-function [distout] = compute_mwdist(molarr,column_num)
+function [distout] = compute_mwdist(dataarr,column_num)
 
 % To compute the molecular weight distribution.
 % Inputs: array name and the column number for which distn needs to be
@@ -7,6 +7,7 @@ function [distout] = compute_mwdist(molarr,column_num)
 % Keep bin width to 8.
 % Filter out ZERO molecular weight
 
-[~,~,filt_dist] = find(molarr(:,column_num));
+[~,~,filt_dist] = find(dataarr(:,column_num));
 binwid    = 8;
 distout   = histogram(filt_dist,'BinWidth',binwid,'Normalization','pdf');
+% distout.Values
