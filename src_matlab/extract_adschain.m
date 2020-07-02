27852a221955dd9b334022e1943974d6c438f912
vaidyanathanms <msvaidyanathan1729@gmail.com>
2020-07-02 00:50:34 -0400

    now it returns the number of frames and the array containing the values in  the freechainMW column

--- src_matlab/extract_adschain.m
+++ src_matlab/extract_adschain.m
@@ -1,10 +1,13 @@
-function [adschain_arr] = extract_adschain(main_fylename,max_mw_ads)
+function [adschain_arr,ads_mws_arr,n_frames] = extract_adschain(main_fylename,max_mw_ads)
 
 % Required header in main_fyle: "freechainMW" with comma separated 
 % ONLY HEADER IS COMMA DELIMITED
+% First column should be the frame ID. Frame IDs should be positive
+% integers.
 
 % Check whether file exists
 fid = fopen(main_fylename,'r');
+n_frames = 0;
 
 if fid <= 0
     fprintf('%s not found \n',main_fylename);
@@ -15,6 +18,7 @@ end
 fprintf('Analyzing %s\n', main_fylename);
 adschain_arr = zeros(max_mw_ads,2); % Maximum value of the MW that can be adsorbed
 adschain_arr(:,1) = 1:max_mw_ads; % Transpose is automatically taken care of by MATLAB
+ads_mws_arr = zeros(1000,1); % adsorbed mol weights
 
 % Start reading file and find header keyword
 tline = fgetl(fid); % get header
@@ -36,17 +40,30 @@ if find_keyword == -1
     return;
 end
 
+if column_num == 1
+    fprintf('ERROR: First column should be frame ID %s\n',tline);
+    return;
+end
+tval_old = -1;
+line_cntr = 0;
 % start reading the rest of the file
 while true
     
     tline = fgetl(fid); % get header
     if ~ischar(tline) || isempty(tline)
-        continue;
+        break;
     end
     
     spl_tline = strtrim(strsplit(strtrim(tline)));
-    ch_mw = str2double(spl_tline{column_num});
     
+    tval_new = str2double(spl_tline{1});
+    if tval_new ~= tval_old
+        n_frames = n_frames+1;
+        tval_old = tval_new;
+    end
+
+    line_cntr = line_cntr + 1;
+    ch_mw = str2double(spl_tline{column_num});
     % check if this exceeds the max designated value
     if ch_mw > max_mw_ads 
         fprintf('ERROR: MW of ads_chains (%d) exceeds the max value (%d). Change "max_mw_free" value in the "Pre-calculations" section',...
@@ -61,7 +78,15 @@ while true
     end
     
     adschain_arr(ch_mw,2) = adschain_arr(ch_mw,2) + 1; % add corresponding value
+    ads_mws_arr(line_cntr,1) = ch_mw;
         
+end
+
+ads_mws_arr = ads_mws_arr(ads_mws_arr~=0); % Find all non-zero elements in case the size of the array is less than 1000 (default)
+if length(ads_mws_arr(:,1)) ~= line_cntr % sanity check
+    fprintf('ERROR: Number of elements in adsorbed mol wt array not equal to the number of lines in the file: %d\t%d\n',...
+        length(ads_mws_arr(:,1)),line_cntr);
+    return;
 end
     
     
