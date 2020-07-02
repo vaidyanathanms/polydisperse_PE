0e99fa25327190bc9f01c2e3c81a556f17a73b05
vaidyanathanms <msvaidyanathan1729@gmail.com>
2020-07-02 19:42:20 -0400

    added more comments and changed norm_distcounts to a 4D array from a 2D array

--- src_matlab/find_distribution_of_mw.m
+++ src_matlab/find_distribution_of_mw.m
@@ -10,11 +10,12 @@ function [norm_distcounts,sorted_init_uniq_arr] = find_distribution_of_mw(dataar
 % total_frames: total number of frames analyzed (summing across all the
 % cases and num_fyles per case for a given arch.)
 
-%Outputs: probdist - 2D array containing the unique MWs of the initial MW
-%distribution array (init_dist) and the normalized number of counts of the
-%adsorbed chains MW
-%distout: contains the normalized probability distribution of probdist with
-%the prescribed binwidth
+%Outputs: norm_distcounts (4D array): col1: list of all initial MWs.
+%identical to the first column of sorted_init_uniq_arr; col2: total
+%occurence of a given mol wt; col3: total occurence per frame; col4: total
+%occurence/frame/inital number of chains with a given MW
+%sorted_init_uniq_arr: (2D array): col1: list of all initial MWs, col2:
+%occurence of each MW at the beginning. sum(col2)=num chains
 %% Analysis
 
 % Counts of initial MW distribution: https://www.mathworks.com/help/matlab/ref/unique.html
@@ -37,7 +38,7 @@ sorted_adsb_uniq_arr = sortrows(adsorbed_unique_counts,1);
 % occurences per MW. Then divide by the number of frames to normalize.
 % p(n_ads^mw) = [\sum_{i=1}^{n_f^mw} n_i,ads^MW \times num_frames]/[n_f^MW
 % \times \sum N_f]
-norm_distcounts = zeros(length(sorted_init_uniq_arr),2);
+norm_distcounts = zeros(length(sorted_init_uniq_arr),3);
 norm_distcounts(:,1) = sorted_init_uniq_arr(:,1);
 
 for i = 1:length(sorted_adsb_uniq_arr)
@@ -45,7 +46,7 @@ for i = 1:length(sorted_adsb_uniq_arr)
     adsorbed_num_chains = sorted_adsb_uniq_arr(i,2);
     find_in_init = -1;
     
-    for j = 1:length(sorted_init_uniq_arr)
+    for j = 1:length(sorted_init_uniq_arr) %find which element in init array does this correspond to
         init_MW = sorted_init_uniq_arr(j,1);
         
         if adsorb_MW == init_MW
@@ -62,11 +63,9 @@ for i = 1:length(sorted_adsb_uniq_arr)
         return;
     end
     
-    norm_distcounts(init_index,2) = adsorbed_num_chains/(init_num_chains*total_frames); 
-end
+    norm_distcounts(init_index,2) = adsorbed_num_chains; %total occurence
+    norm_distcounts(init_index,3) = adsorbed_num_chains/total_frames; %total occurence per frame
+    norm_distcounts(init_index,4) = adsorbed_num_chains/(init_num_chains*total_frames); %total occurence/frame/initial number of same MW
     
-           
-
-
-
+end
 
