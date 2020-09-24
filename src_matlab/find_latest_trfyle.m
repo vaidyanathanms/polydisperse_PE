f399fa83912f250cf68d5bc07afca37220d0a5d2
vaidyanathanms <msvaidyanathan1729@gmail.com>
2020-09-24 14:58:14 -0400

    bug fix in for loop

--- src_matlab/find_latest_trfyle.m
+++ src_matlab/find_latest_trfyle.m
@@ -9,7 +9,7 @@ end
 tstamp_new = str2double(allstrings{posit});
 ind_latest = 1;
 for i = 2:nfyles
-    allstrings = strsplit(char(inplist{1,1}),{'_','.'});
+    allstrings = strsplit(char(inplist{i,1}),{'_','.'});
     tstamp = str2double(allstrings{posit});
     if tstamp > tstamp_new
         tstamp_new = tstamp;
