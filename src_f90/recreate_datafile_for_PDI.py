0e489aecf22b4cdb18f61a87a62e909f0a9082b6
vaidyanathanms <msvaidyanthan1729@gmail.com>
2021-03-08 16:40:42 -0600

    updated extra py files

--- src_f90/recreate_datafile_for_PDI.py
+++ src_f90/recreate_datafile_for_PDI.py
@@ -17,16 +17,16 @@ from my_python_functions import my_cpy_generic
 analysis_type  = 2 #1-polydisp_pe,2-newparams,3-mwchange,#4-oldsize
 
 #---------input details----------------------------------------
-free_chains  = [16,32,64,128,150]
+free_chains  = [150] #[16,32,64,128,150]
 free_avg_mw  = 30
 graft_chains = 32
 graft_avg_mw = 35 
 tail_mons    = 5
 nsalt        = 510
 f_charge     = 0.5
-archarr      = [1]
+archarr      = [4]
 ncases_pdi   = [1,2,3,4]
-pdi_free     = 1.5
+pdi_free     = 1.0
 pdi_graft    = 1.0
 cutoff_dist  = 1.50 #use two decimal places
 
