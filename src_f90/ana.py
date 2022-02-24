4b82c139a0fb3116be6f574f97a91d31a0937164
vaidyanathanms <msvaidyanthan1729@gmail.com>
2022-02-24 15:51:23 -0600

    updated for n=0

--- src_f90/ana.py
+++ src_f90/ana.py
@@ -20,15 +20,15 @@ from my_python_functions import run_analysis
 analysis_type = 2 #1-polydisp_pe,2-newparams,3-mwchange,#4-oldsize
 
 #---------input details----------------------------------------
-free_chains  = [16,32,64,128,150]
+free_chains  = [150]
 free_avg_mw  = 30
 graft_chains = 32
 graft_avg_mw = 35 
 tail_mons    = 5
 nsalt        = 510
 f_charge     = 0.5
-archarr      = [1,4]
-ncases_pdi   = [1,2,3,4]
+archarr      = [3]
+ncases_pdi   = [3]
 pdi_free     = 1.0
 pdi_graft    = 1.0
 cutoff_dist  = 1.50 #use two decimal places
